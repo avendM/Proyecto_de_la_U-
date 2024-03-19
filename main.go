d9c0d66c794b4496bbe004a28240912384e971f2
avendM <164035915+avendM@users.noreply.github.com>
2024-03-19 10:02:26 -0600

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1-Motocicleta ")
-	fmt.Println("2- Liviano ")
-	fmt.Println("3- Carga Liviana ")
-	fmt.Println("4- Carga Pesada ")
-	fmt.Println("5- Agrícola ")
-	fmt.Println("6- - Casa Rodante ")
-	fmt.Println("7- Buses ")
-	fmt.Println("8- Regresar a menú principal ")
+	var num int
+	fmt.Println("1-consultar tarifa")
+	fmt.Println("2-cobro de peaje")
+	fmt.Println("3-cambiar tarifa")
+	fmt.Println("4-mostras tarifas")
+	fmt.Println("5-salir")
+	fmt.Scanln(&num)
 }
